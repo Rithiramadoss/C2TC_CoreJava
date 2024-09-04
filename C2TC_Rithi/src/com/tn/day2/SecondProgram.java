89353b4176d90d27155bbb10c40a5e465dde31cc
ASUS <ASUS@Rithika-Ramadoss03m>
2024-09-04 14:52:07 +0530

    for loop

---
+++ C2TC_Rithi/src/com/tn/day2/SecondProgram.java
@@ -0,0 +1,11 @@
+package com.tn.day2;
+
+ public class SecondProgram {
+	public static void main (String[]args) {
+  for (int i = 1; i <= 10; i++) {
+         System.out.println("Number: " + i);
+     }
+ }
+}
+
+
